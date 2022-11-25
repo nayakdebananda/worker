591cbeb27a1686d3ac0627840c623fb45dd9ed66
subhransuBarik <116339604+subhransuBarik@users.noreply.github.com>
2022-11-25 15:05:18 +0530

    operator updated

--- main.sh
+++ main.sh
@@ -3,14 +3,14 @@ filename=$2
 inputfile=$3
 outputfile=${filename%.*}
 
-if [ "$compiler" == "g++" ]
+if [ "$compiler" = "g++" ]
 then
     if   $(g++ -o $outputfile $filename)
     then
         ./$outputfile < $inputfile
     fi
 exit
-elif [ "$compiler" == "javac" ]
+elif [ "$compiler" = "javac" ]
 then
     if $(javac $filename) 
     then
@@ -21,9 +21,3 @@ fi
 
 $compiler $filename < $inputfile
 
-# will take 2 arguements
-# 1--> compiler name
-# 2--> source code
-# 3--> input file contain required input to execut the program
-# run command -> [extcute shell program file] g++ main.cpp input
-#           or  sh main.sh g++ main.cpp input
